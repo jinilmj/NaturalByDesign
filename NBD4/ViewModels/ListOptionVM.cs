4690b967e1961812191d0414affe54ef211d2356
Jinil M J <mjjinil@gmail.com>
2024-03-01 12:17:40 -0500

    updated

---
+++ NBD4/ViewModels/ListOptionVM.cs
@@ -0,0 +1,11 @@
+﻿namespace NBD4.ViewModels
+{
+    public class ListOptionVM
+    {
+        public int ID { get; set; }
+        public string DisplayText { get; set; }
+        public int Quantity { get; set; }
+        public double Price { get; set; }
+
+    }
+}
